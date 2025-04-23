35aee9029a5b30d979f3c4dbc71470cb0d4e47fe
Champii1 <contact@champii.io>
2025-04-23 14:12:48 +0200

    Max scratchpad size == 2M

--- mutant-lib/src/index/mod.rs
+++ mutant-lib/src/index/mod.rs
@@ -4,7 +4,7 @@ pub mod pad_info;
 
 /// Default size for scratchpads in bytes (4 MiB minus one page for metadata).
 // pub(crate) const DEFAULT_SCRATCHPAD_SIZE: usize = (4 * 1024 * 1024) - 4096;
-pub(crate) const DEFAULT_SCRATCHPAD_SIZE: usize = (1 * 1024 * 1024);
+pub(crate) const DEFAULT_SCRATCHPAD_SIZE: usize = (2 * 1024 * 1024);
 
 pub(crate) use pad_info::{PadInfo, PadStatus};
 
